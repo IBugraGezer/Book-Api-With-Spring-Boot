aa5e8d84a3606e08874bc80b17a1bdbc11b7fc32
Burra <gezerbugra@gmail.com>
2022-12-09 00:58:35 +0300

    exceptions added to AuthorService

--- src/main/java/com/burra/business/services/abstracts/AuthorService.java
+++ src/main/java/com/burra/business/services/abstracts/AuthorService.java
@@ -2,6 +2,8 @@ package com.burra.business.services.abstracts;
 
 import java.util.ArrayList;
 
+import com.burra.business.exceptions.DataAlreadyExistsException;
+import com.burra.business.exceptions.ResourceNotFoundException;
 import com.burra.business.requests.author.CreateAuthorRequest;
 import com.burra.business.requests.author.UpdateAuthorRequest;
 import com.burra.business.responses.author.CreateAuthorResponse;
@@ -11,7 +13,7 @@ import com.burra.entities.Author;
 public interface AuthorService {
   public ArrayList<Author> getAll();
 
-  public CreateAuthorResponse create(CreateAuthorRequest request);
+  public CreateAuthorResponse create(CreateAuthorRequest request) throws DataAlreadyExistsException;
 
-  public UpdateAuthorResponse update(UpdateAuthorRequest request, int id);
+  public UpdateAuthorResponse update(UpdateAuthorRequest request, int id) throws ResourceNotFoundException;
 }
