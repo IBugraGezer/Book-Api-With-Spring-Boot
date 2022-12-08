d0d74cd35c8238ac450f2541c5afccb104f58d35
Burra <gezerbugra@gmail.com>
2022-12-08 13:30:24 +0300

    Book entity created

---
+++ src/main/java/com/burra/entities/Book.java
@@ -0,0 +1,52 @@
+package com.burra.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "books")
+public class Book {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id")
+  private int id;
+
+  @Column(name = "name")
+  private String name;
+
+  @ManyToOne
+  @JoinColumn(name = "author_id", nullable = false)
+  private Author author;
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public Author getAuthor() {
+    return author;
+  }
+
+  public void setAuthor(Author author) {
+    this.author = author;
+  }
+
+}
