6229c58ffa6f052213f35f4534f78be6a61dcf7e
Burra <gezerbugra@gmail.com>
2022-12-09 00:52:54 +0300

    update method added to AuthorService

--- src/main/java/com/burra/business/services/abstracts/AuthorService.java
+++ src/main/java/com/burra/business/services/abstracts/AuthorService.java
@@ -3,11 +3,15 @@ package com.burra.business.services.abstracts;
 import java.util.ArrayList;
 
 import com.burra.business.requests.author.CreateAuthorRequest;
+import com.burra.business.requests.author.UpdateAuthorRequest;
 import com.burra.business.responses.author.CreateAuthorResponse;
+import com.burra.business.responses.author.UpdateAuthorResponse;
 import com.burra.entities.Author;
 
 public interface AuthorService {
   public ArrayList<Author> getAll();
 
   public CreateAuthorResponse create(CreateAuthorRequest request);
+
+  public UpdateAuthorResponse update(UpdateAuthorRequest request);
 }
