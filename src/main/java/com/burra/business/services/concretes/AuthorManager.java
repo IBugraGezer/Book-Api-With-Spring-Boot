56c4c044c34d638c087b7f6d833f9ddbf597abf0
Burra <gezerbugra@gmail.com>
2022-12-09 00:38:28 +0300

    AuthorManager created

---
+++ src/main/java/com/burra/business/services/concretes/AuthorManager.java
@@ -0,0 +1,40 @@
+package com.burra.business.services.concretes;
+
+import java.util.ArrayList;
+
+import org.springframework.stereotype.Service;
+
+import com.burra.business.exceptions.DataAlreadyExistsException;
+import com.burra.business.requests.CreateAuthorRequest;
+import com.burra.business.responses.CreateAuthorResponse;
+import com.burra.business.services.abstracts.AuthorService;
+import com.burra.dataAccess.abstracts.AuthorRepository;
+import com.burra.entities.Author;
+
+@Service
+public class AuthorManager implements AuthorService {
+
+  private AuthorRepository authorRepository;
+
+  public AuthorManager(AuthorRepository authorRepository) {
+    this.authorRepository = authorRepository;
+  }
+
+  @Override
+  public ArrayList<Author> getAll() {
+    return (ArrayList<Author>) authorRepository.findAll();
+  }
+
+  @Override
+  public CreateAuthorResponse create(CreateAuthorRequest request) {
+    if (authorRepository.existsByName(request.getName())) {
+      throw new DataAlreadyExistsException("Bu isimde bir yazar zaten var");
+    }
+    Author author = new Author();
+    author.setName(request.getName());
+    Author newAuthor = authorRepository.save(author);
+
+    return new CreateAuthorResponse(newAuthor);
+  }
+
+}
