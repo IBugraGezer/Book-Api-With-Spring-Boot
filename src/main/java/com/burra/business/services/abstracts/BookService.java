2d4137e62c9a94eae7ed916f48b261be7e9b259d
Burra <gezerbugra@gmail.com>
2022-12-10 00:51:42 +0300

    update method added to BookService

--- src/main/java/com/burra/business/services/abstracts/BookService.java
+++ src/main/java/com/burra/business/services/abstracts/BookService.java
@@ -5,7 +5,9 @@ import java.util.ArrayList;
 import com.burra.business.exceptions.DataAlreadyExistsException;
 import com.burra.business.exceptions.ResourceNotFoundException;
 import com.burra.business.requests.book.CreateBookRequest;
+import com.burra.business.requests.book.UpdateBookRequest;
 import com.burra.business.responses.book.CreateBookResponse;
+import com.burra.business.responses.book.UpdateBookResponse;
 import com.burra.entities.Book;
 
 public interface BookService {
@@ -14,5 +16,7 @@ public interface BookService {
 
   public CreateBookResponse create(CreateBookRequest request)
       throws DataAlreadyExistsException, ResourceNotFoundException;
+
+  public UpdateBookResponse update(UpdateBookRequest request, int id) throws ResourceNotFoundException;
 
 }
