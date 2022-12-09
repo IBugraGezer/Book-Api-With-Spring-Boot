1e95b54994fc31c9915b759d2557196622a0408d
Burra <gezerbugra@gmail.com>
2022-12-10 01:04:41 +0300

    update method defined at BookController

--- src/main/java/com/burra/webApi/BookController.java
+++ src/main/java/com/burra/webApi/BookController.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +14,9 @@ import org.springframework.web.bind.annotation.RestController;
 import com.burra.business.exceptions.DataAlreadyExistsException;
 import com.burra.business.exceptions.ResourceNotFoundException;
 import com.burra.business.requests.book.CreateBookRequest;
+import com.burra.business.requests.book.UpdateBookRequest;
 import com.burra.business.responses.book.CreateBookResponse;
+import com.burra.business.responses.book.UpdateBookResponse;
 import com.burra.business.services.abstracts.BookService;
 import com.burra.entities.Book;
 
@@ -39,4 +43,10 @@ public class BookController {
     return ResponseEntity.ok().body(response);
   }
 
+  @PutMapping("/{id}")
+  public ResponseEntity<UpdateBookResponse> update(@Valid @RequestBody UpdateBookRequest request, @PathVariable int id)
+      throws ResourceNotFoundException {
+    UpdateBookResponse response = bookService.update(request, id);
+    return ResponseEntity.ok().body(response);
+  }
 }
