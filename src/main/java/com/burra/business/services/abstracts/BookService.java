b0a7de696db9968abdb808570a519171d8d7aaa4
Burra <gezerbugra@gmail.com>
2022-12-09 16:50:01 +0300

    ResourceNotFoundException added to create method of BookService

--- src/main/java/com/burra/business/services/abstracts/BookService.java
+++ src/main/java/com/burra/business/services/abstracts/BookService.java
@@ -3,6 +3,7 @@ package com.burra.business.services.abstracts;
 import java.util.ArrayList;
 
 import com.burra.business.exceptions.DataAlreadyExistsException;
+import com.burra.business.exceptions.ResourceNotFoundException;
 import com.burra.business.requests.book.CreateBookRequest;
 import com.burra.business.responses.book.CreateBookResponse;
 import com.burra.entities.Book;
@@ -11,6 +12,7 @@ public interface BookService {
 
   public ArrayList<Book> getAll();
 
-  public CreateBookResponse create(CreateBookRequest request) throws DataAlreadyExistsException;
+  public CreateBookResponse create(CreateBookRequest request)
+      throws DataAlreadyExistsException, ResourceNotFoundException;
 
 }
