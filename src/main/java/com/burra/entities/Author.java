4bca7cc674f357fd2fbf509adfc124dbb29bf76e
Burra <gezerbugra@gmail.com>
2022-12-09 17:10:12 +0300

    books field defined at Author

--- src/main/java/com/burra/entities/Author.java
+++ src/main/java/com/burra/entities/Author.java
@@ -1,10 +1,13 @@
 package com.burra.entities;
 
+import java.util.ArrayList;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -18,6 +21,9 @@ public class Author {
 
   @Column(name = "name")
   private String name;
+
+  @OneToMany(mappedBy = "authors")
+  private ArrayList<Book> books;
 
   public int getId() {
     return id;
