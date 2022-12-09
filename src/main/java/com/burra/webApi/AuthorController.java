476184687e0925271a0bc5ab62e82a14e3eeaf3a
Burra <gezerbugra@gmail.com>
2022-12-09 16:33:33 +0300

    AuthorController created

--- src/main/java/com/burra/webApi/AuthorController.java
+++ src/main/java/com/burra/webApi/AuthorController.java
@@ -4,15 +4,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.burra.business.exceptions.DataAlreadyExistsException;
+import com.burra.business.exceptions.ResourceNotFoundException;
 import com.burra.business.requests.author.CreateAuthorRequest;
+import com.burra.business.requests.author.UpdateAuthorRequest;
 import com.burra.business.responses.author.CreateAuthorResponse;
+import com.burra.business.responses.author.UpdateAuthorResponse;
 import com.burra.business.services.abstracts.AuthorService;
 import com.burra.entities.Author;
 
@@ -37,13 +43,25 @@ public class AuthorController {
   @PostMapping("")
   public ResponseEntity<CreateAuthorResponse> create(@Valid @RequestBody CreateAuthorRequest request)
       throws DataAlreadyExistsException {
-    try {
-      CreateAuthorResponse response = authorService.create(request);
-      return ResponseEntity.ok().body(response);
-    } catch (DataAlreadyExistsException e) {
-      throw e;
-    }
+    CreateAuthorResponse response = authorService.create(request);
+    return ResponseEntity.ok().body(response);
 
+  }
+
+  @PutMapping("/{id}")
+  public ResponseEntity<UpdateAuthorResponse> update(@Valid @RequestBody UpdateAuthorRequest request,
+      @PathVariable int id)
+      throws ResourceNotFoundException {
+
+    UpdateAuthorResponse response = authorService.update(request, id);
+    return ResponseEntity.ok().body(response);
+  }
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<String> delete(@PathVariable int id) throws ResourceNotFoundException {
+    authorService.delete(id);
+
+    return ResponseEntity.ok().body("silindi");
   }
 
 }
